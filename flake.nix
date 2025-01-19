17f61eefbd8fd60a3ff6365db6b3ce85c32a6cbb
Sayantan Khan <sayantangkhan@gmail.com>
2025-01-19 17:16:57 -0500

    Moved to uv/nix combined setup

--- flake.nix
+++ flake.nix
@@ -1,52 +1,28 @@
+# in flake.nix
 {
-  description = "A flake to manage and build my website.";
-
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        python_packages = pkgs.python3.withPackages(ps: with ps;[
-          pelican
-          black
-          blinker
-          click
-          commonmark
-          docutils
-          feedgenerator
-          jinja2
-          markdown
-          markupsafe
-          mypy-extensions
-          pathspec
-          platformdirs
-          pygments
-          python-dateutil
-          pytz
-          rich
-          six
-          tomli
-          unidecode
-          pelican-render-math
-        ]);
-      in
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        with pkgs;
         {
-          packages.website = pkgs.stdenv.mkDerivation rec {
+          devShells.default = mkShell {
+            buildInputs = [ uv ];
+          };
+          packages.default = pkgs.stdenv.mkDerivation rec {
             pname = "static-website";
-            version = "2022-11-18";
+            version = "2025-01-19";
             src = ./.;
-            nativeBuildInputs = [ python_packages ];
-            buildPhase = "make html";
+            nativeBuildInputs = [ uv ];
+            buildPhase = "uv run -- make html";
             installPhase = "cp -r output $out";
           };
-          defaultPackage = self.packages.${system}.website;
-          devShell = pkgs.mkShell {
-            packages = with pkgs; [
-              python_packages
-            ];
-          };
         }
-    );
+      );
 }
